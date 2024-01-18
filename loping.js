d28a80b2c0d278f7954663431302381e77add3f3
Ditaaa <113565619+ditanursabila@users.noreply.github.com>
2024-01-18 21:35:07 +0700

    Delete loping.js

--- loping.js
+++
@@ -1,9 +0,0 @@
-let teenagers = ["mardiyani", "felisha", "sabila", "triani", "jamilah", "tsani"];
-let tampil = teenagers.length;
-
-let text="";
-for (let i = 0; i < tampil; i++) {
-  text += "<li>" + teenagers[i] + "</li>";
-}
-
-document.getElementById("kita").innerHTML = text;
